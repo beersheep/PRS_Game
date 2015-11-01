d40c38014aa2e4dbc8eb334cee4acc0f2148b28d
beersheep <beersheep0109@gmail.com>
2015-11-01 20:33:16 +0800

    fix some identation and warp up with methods

--- PRS_game.rb
+++ PRS_game.rb
@@ -2,63 +2,61 @@ require 'pry'
 
 CHOICES = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
 
-#1. Display Playing Message
+def display_welcome_message
+  puts "=> Welcome to the Paper, Rock, Scissors Game!"
+end
 
-puts "=> Welcome to the Paper, Rock, Scissors Game!"
+def print_choice(who, move)
+  case move 
+  when "p"
+    puts "=>#{who} chose Paper!"
+  when "r"
+    puts "=>#{who} chose Rock!"
+  when "s"
+    puts "=>#{who} chose Scissor!"
+  end
+end
 
+def display_result(player_move, computer_move)
+  if player_move == computer_move 
+    puts "It's a tie!"
+  elsif (player_move == "p" && computer_move == "r") || 
+        (player_move == "r" && computer_move == "s") ||
+        (player_move == "s" && computer_move == "p")     
+    puts "You win!"
+  else 
+    puts "You lose!"
+  end
+end
 
-#2. Choose your move
+def get_player_choice
+  move = ''
+  begin 
+    puts "Please choose your move: (P/R/S)"
+    move = gets.chomp.downcase
+  end until CHOICES.keys.include?(move)
+  move
+end
+
+def get_computer_choice
+   CHOICES.keys.sample
+end
+
+def play_again?
+   puts "Would you like to play again?(Y/N)"
+   gets.chomp.downcase == "y"
+end
+
 loop do
 
-# until ("p").include?(player_move) ||
-#       ("r").include?(player_move) ||
-#       ("s").include?(player_move)
-#   puts "Please choose between [P]aper, [S]cissors and [R]ock!"
-#   player_move = gets.chomp.downcase
-# end
+ 
+  player_move = get_player_choice
+  print_choice("You", player_move)
+ 
+  computer_move = get_computer_choice
+  print_choice("Computer", computer_move)
 
-begin 
-  puts "Please choose your move: (P/R/S)"
-  player_move = gets.chomp.downcase
-end until CHOICES.keys.include?(player_move)
-
-case player_move 
-  when "p"
-    puts "=>You chose Paper!"
-  when "r"
-    puts "=>You chose Rock!"
-  when "s"
-    puts "=>You chose Scissor!"
-end
-
-#3. Randomly choose computer's move
-
-computer_move = CHOICES.keys.sample
-
-case computer_move 
-  when "p"
-    puts "=>Computer chose Paper!"
-  when "r"
-    puts "=>Computer chose Rock!"
-  when "s"
-    puts "=>Computer chose Scissor!"
-end
-
-#4. Display the result
-
-if player_move == computer_move 
-  puts "It's a tie!"
-elsif (player_move == "p" && computer_move == "r") || 
-      (player_move == "r" && computer_move == "s") ||
-      (player_move == "s" && computer_move == "p")     
-  puts "You win!"
-else 
-  puts "You lose!"
-end
-
-#5. Play Again?
-
-puts "Would you like to play again?(Y/N)"
-break if gets.chomp.downcase != "y"
-
+  display_result(player_move, computer_move)
+  break if !play_again?
+  
 end
